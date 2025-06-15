2f1e151c053bd4caf716d3fbdac68a9feb3e3bdc
Bhupendra Sharma <bhupendrasharma3423@gmail.com>
2025-06-15 17:44:46 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,3 +15,7 @@ app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/", (req, res) => {
+  res.send("SaaS Backend Working ✅");
+});
+
